344fe33b91911d14034f66e7bf12d84f79b1081f
Victor Ferraz <victor.fzn7@gmail.com>
2019-08-05 11:33:34 -0300

    Diferentes maneiras de usar o Console

---
+++ Console/clog.js
@@ -0,0 +1,10 @@
+let foo  = { nome: 'Zé', idade: 30, nervoso: true}
+let bar = { nome: 'Ronaldo', idade: 40, nervoso: false}
+let baz = { nome: 'Zezo', idade: 25, nervoso: false}
+
+// console.log(foo)
+// console.log(bar)
+// console.log(baz)
+
+console.log('%c Mlkada' , 'color: orange; font-weight: bolder;')
+console.log({foo, bar, baz})
---
+++ Console/ctable.js
@@ -0,0 +1,11 @@
+let foo  = { nome: 'Zé', idade: 30, nervoso: true}
+let bar = { nome: 'Ronaldo', idade: 40, nervoso: false}
+let baz = { nome: 'Zezo', idade: 25, nervoso: false}
+
+
+console.table([foo, bar, baz])
+
+/*
+Console.table: Exibe os dados como uma tabela
+https://developer.mozilla.org/en-US/docs/Web/API/Console/table
+*/
